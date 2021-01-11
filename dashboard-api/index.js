914bf2c8dc7ecfcbe026bc3acd46a5ac8dfef61f
Munkhsaikhan Ganchimeg <munkhsaikhan.g@nmma.co>
2021-01-11 16:56:06 +0800

    merge develop into golomt on dashboard

--- dashboard-api/index.js
+++ dashboard-api/index.js
@@ -1,11 +1,13 @@
 const CubejsServer = require('@cubejs-backend/server');
 const dotenv = require('dotenv');
 const generateReport = require('./controller/controller.js');
+const generateQuery = require('./controller/generator.js');
 const ModifiedElasticSearchDriver = require('./driver.js');
+const jwt = require('jsonwebtoken');
 
 dotenv.config();
 
-const { SCHEMA_PATH } = process.env;
+const { SCHEMA_PATH, CUBEJS_API_SECRET } = process.env;
 
 const server = new CubejsServer({
   schemaPath: SCHEMA_PATH || '/schema',
@@ -14,6 +16,10 @@ const server = new CubejsServer({
       xpack: true,
       dataSource
     });
+  },
+  queryTransformer: async query => {
+    await generateQuery(query);
+    return query;
   }
 });
 
@@ -21,6 +27,15 @@ server
   .listen()
   .then(({ app, port }) => {
     app.get('/get', (req, res) => generateReport(req, res));
+    app.get('/get-token', (req, res) => {
+      const dashboardToken = jwt.sign({}, CUBEJS_API_SECRET || 'secret', {
+        expiresIn: '10day'
+      });
+
+      return res.send({
+        dashboardToken: dashboardToken
+      });
+    });
 
     console.log(`🚀 Cube.js server is listening on ${port}`);
   })
